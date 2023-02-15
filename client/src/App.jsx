9ca2fa98bba61c0eb9af66afa735b40cddedc7dd
Priyam <priyam05soni@gmail.com>
2023-02-15 12:03:37 +0530

    client side

---
+++ client/src/App.jsx
@@ -0,0 +1,53 @@
+import axios from "axios";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Layout from "./components/Layout";
+import UserContextProvider from "./context/UserContext";
+import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/LoginPage";
+import PlacesFormPage from "./pages/PlacesFormPage";
+import PlacesPage from "./pages/PlacesPage";
+import ProfilePage from "./pages/ProfilePage";
+import RegisterPage from "./pages/RegisterPage";
+import PlacePage from "./pages/PlacePage";
+import BookingsPage from "./pages/BookingsPage";
+import BookingPage from "./pages/BookingPage";
+
+// axios.defaults.baseURL = "http://localhost:4000";
+axios.defaults.baseURL = "http://127.0.0.1:4000";
+axios.defaults.withCredentials = true;
+
+function App() {
+    return (
+        <UserContextProvider>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<IndexPage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/register" element={<RegisterPage />} />
+                    <Route path="/account" element={<ProfilePage />} />
+                    <Route path="/account/places" element={<PlacesPage />} />
+                    <Route
+                        path="/account/places/new"
+                        element={<PlacesFormPage />}
+                    />
+                    <Route
+                        path="/account/places/:id"
+                        element={<PlacesFormPage />}
+                    />
+                    <Route path="/place/:id" element={<PlacePage />} />
+                    <Route
+                        path="/account/bookings"
+                        element={<BookingsPage />}
+                    />
+                    <Route
+                        path="/account/bookings/:id"
+                        element={<BookingPage />}
+                    />
+                </Route>
+            </Routes>
+        </UserContextProvider>
+    );
+}
+
+export default App;
